7564f07e6d87c55871ce31b122ea1efec37c8b15
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2025-12-03 17:51:41 +0100

    fix typo

---
+++ docs/assets/Oriane_Secrétaire-Bdz4oFH6.js
@@ -0,0 +1,1 @@
+const e=""+new URL("Oriane_Secrétaire-DZtLZ-4w.png",import.meta.url).href;export{e as default};
--- docs/assets/Oriane_Sécrétaire-D8Q9pPCB.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("Oriane_Sécrétaire-DZtLZ-4w.png",import.meta.url).href;export{e as default};
